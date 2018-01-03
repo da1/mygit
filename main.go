9b3000b5392d72d057bc37745bc6051feb7808ed
da1 <dyamada1988@gmail.com>
2018-01-03 12:14:58 +0900

    update
    addIndex実装，リファクタ

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"flag"
 	"fmt"
 	"io"
@@ -9,6 +11,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 type Object struct {
@@ -43,17 +46,51 @@ func parseBlob(obj Object) (*BlobObject, error) {
 	return &BlobObject{contentSize, data2}, nil
 }
 
+func saveBlobObject(blob BlobObject) error {
+	data := fmt.Sprintf("blob %d%s%s", blob.Size, string([]byte{0}), blob.Data)
+
+	h := sha1.New()
+	io.WriteString(h, data)
+	sha1 := h.Sum(nil)
+	fmt.Printf("blob %v, sha1: %s", blob, sha1)
+
+	r := strings.NewReader(blob.Data)
+	buf, err := compress(r)
+	if err != nil {
+		return fmt.Errorf("compress error %v", err)
+	}
+
+	filePath := fmt.Sprintf(".git/objects/%s/%s", sha1[0:2], sha1[2:])
+	fmt.Printf("filePath %s\n", filePath)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("create error %v", err)
+	}
+	defer file.Close()
+
+	file.Write(buf.Bytes())
+	return nil
+}
+
+func compress(r io.Reader) (*bytes.Buffer, error) {
+	buf := new(bytes.Buffer)
+	zw := zlib.NewWriter(buf)
+	defer zw.Close()
+
+	if _, err := io.Copy(zw, r); err != nil {
+		return buf, err
+	}
+	return buf, nil
+}
+
 func extract(zr io.Reader) (io.Reader, error) {
 	return zlib.NewReader(zr)
 }
 
-func main() {
-	p := flag.String("p", "", "p")
-	debug := flag.Bool("d", false, "debug")
-	flag.Parse()
-
-	filePath := fmt.Sprintf(".git/objects/%s/%s", (*p)[0:2], (*p)[2:])
-	if *debug {
+func catFile(hash string, debug bool) {
+	filePath := fmt.Sprintf(".git/objects/%s/%s", hash[0:2], hash[2:])
+	if debug {
 		fmt.Printf("file path %s\n", filePath)
 	}
 
@@ -74,7 +111,7 @@ func main() {
 		return
 	}
 
-	if *debug {
+	if debug {
 		fmt.Printf("read %s\n", b)
 	}
 
@@ -85,6 +122,57 @@ func main() {
 		fmt.Printf("%v", err)
 		return
 	}
+	if debug {
+		fmt.Printf("%v\n", blobObject)
+	}
 
 	fmt.Printf("%s", blobObject.Data)
+}
+
+func addIndex(fileName string, debug bool) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("file can't open %s %v\n", fileName, err)
+		return
+	}
+	defer f.Close()
+
+	bufferSize := 1024
+	data := ""
+	buf := make([]byte, bufferSize)
+	for {
+		n, err := f.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			fmt.Printf("%v", err)
+			return
+		}
+		data += string(buf[:n])
+	}
+
+	if debug {
+		fmt.Println(data)
+	}
+
+	blob := BlobObject{utf8.RuneCountInString(data), data}
+	if debug {
+		fmt.Printf("%v\n", blob)
+	}
+}
+
+func main() {
+	p := flag.String("p", "", "p")
+	add := flag.String("add", "", "add file")
+	debug := flag.Bool("d", false, "debug")
+	flag.Parse()
+
+	if *p != "" {
+		catFile(*p, *debug)
+	}
+
+	if *add != "" {
+		addIndex(*add, *debug)
+	}
 }
